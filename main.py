ee4de8b76f2628f988e53f03f3a172067528349e
KateKateKKK <kate@followup.usgit config --global user.name KateKateKKK>
2025-05-20 10:37:39 +0800

    update

--- main.py
+++ main.py
@@ -210,4 +210,4 @@ async def serve_static(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=9876, log_level="info")
